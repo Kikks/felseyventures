a79845ecebcd5dcc475656246e587995c22d03aa
Kikks <51975725+Kikks@users.noreply.github.com>
2020-03-28 20:08:10 +0100

    Add files via upload

---
+++ build/precache-manifest.e6eff22d3c9dc8e4d5097b4c6aeb6173.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d0b7a8357cdffe0697989e03ab83fcb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a23cbaaa804c4951082d",
+    "url": "/static/css/main.2575aafd.chunk.css"
+  },
+  {
+    "revision": "914d65e8345105c28258",
+    "url": "/static/js/2.7349b392.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.7349b392.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a23cbaaa804c4951082d",
+    "url": "/static/js/main.0c01bf36.chunk.js"
+  },
+  {
+    "revision": "593f1b8b4d73b7f028b9",
+    "url": "/static/js/runtime-main.1ff97197.js"
+  },
+  {
+    "revision": "4b7100bff95526e114633b3125d25516",
+    "url": "/static/media/about-hero.4b7100bf.jpg"
+  },
+  {
+    "revision": "b12fee081f03f3ffc6b8ea7834cdee28",
+    "url": "/static/media/bookings-hero.b12fee08.jpg"
+  },
+  {
+    "revision": "10fc66aa9b2a1657bd1c0923017278ce",
+    "url": "/static/media/chef-hat.10fc66aa.svg"
+  },
+  {
+    "revision": "10ae05c7718a58e7e197b8f6abf33653",
+    "url": "/static/media/chevron-left.10ae05c7.svg"
+  },
+  {
+    "revision": "0d38a7c21b92752ecab94a4f00b6d4c9",
+    "url": "/static/media/chevron-right.0d38a7c2.svg"
+  },
+  {
+    "revision": "d2140291251f623e47d25dca8c62638a",
+    "url": "/static/media/chicken.d2140291.jpg"
+  },
+  {
+    "revision": "08b4ebbc15d75213250e15ea732487bd",
+    "url": "/static/media/contact-hero.08b4ebbc.jpg"
+  },
+  {
+    "revision": "a77b44fe70308572ec1d79025325ce8c",
+    "url": "/static/media/corporate.a77b44fe.jpg"
+  },
+  {
+    "revision": "fd84888334637d6b845db1c2281f888b",
+    "url": "/static/media/facebook-original.fd848883.svg"
+  },
+  {
+    "revision": "db88559900f0078c89e9c4aa461edb87",
+    "url": "/static/media/gallery-hero.db885599.jpg"
+  },
+  {
+    "revision": "4c755057e9a00d1564167716a1292175",
+    "url": "/static/media/heart.4c755057.svg"
+  },
+  {
+    "revision": "2a19105989545dbae803b7f0d79d1118",
+    "url": "/static/media/image1.2a191059.jpg"
+  },
+  {
+    "revision": "76b6e2e5a0265dabaee126ceb95ac659",
+    "url": "/static/media/image10.76b6e2e5.jpg"
+  },
+  {
+    "revision": "e29fe228766e19570acea52180e593aa",
+    "url": "/static/media/image11.e29fe228.jpg"
+  },
+  {
+    "revision": "c156626153c3d0711b6880ab4bbf13a8",
+    "url": "/static/media/image12.c1566261.jpg"
+  },
+  {
+    "revision": "177b9d948f91850c5ea21a0457df49ce",
+    "url": "/static/media/image2.177b9d94.jpg"
+  },
+  {
+    "revision": "9cf4fd50afd649f1d05655d1a34fabbb",
+    "url": "/static/media/image3.9cf4fd50.jpg"
+  },
+  {
+    "revision": "1c6169e44aa79dfc5905c97a939738b6",
+    "url": "/static/media/image4.1c6169e4.jpg"
+  },
+  {
+    "revision": "6f0b17004f6503c3982d84ec9b29fbdc",
+    "url": "/static/media/image5.6f0b1700.jpg"
+  },
+  {
+    "revision": "39f1f744e368d5e9a96a64ad6803a748",
+    "url": "/static/media/image6.39f1f744.jpg"
+  },
+  {
+    "revision": "4e4c50b06e8b261f468233b118ca19b4",
+    "url": "/static/media/image7.4e4c50b0.jpg"
+  },
+  {
+    "revision": "7ae5ca9ebfbdda2653040193dff860b5",
+    "url": "/static/media/image8.7ae5ca9e.jpg"
+  },
+  {
+    "revision": "8c4529865fc99ba3414d5cb2d133b453",
+    "url": "/static/media/image9.8c452986.jpg"
+  },
+  {
+    "revision": "b5177787477d6fb96491ef64b38d0121",
+    "url": "/static/media/instagram-original.b5177787.svg"
+  },
+  {
+    "revision": "b88d4c06fb32f1d2b67774074d779254",
+    "url": "/static/media/reading.b88d4c06.svg"
+  },
+  {
+    "revision": "6715e0f17c4f2765dbc0618c4bcf3f9e",
+    "url": "/static/media/service.6715e0f1.svg"
+  },
+  {
+    "revision": "b76e8dc89a56fd95b061d7fe37fe40a5",
+    "url": "/static/media/settings.b76e8dc8.jpg"
+  },
+  {
+    "revision": "dcac2fd22124587a5c16e07283758d4a",
+    "url": "/static/media/social.dcac2fd2.jpg"
+  },
+  {
+    "revision": "16b020b126e7ac11aa34f75e2e153b5e",
+    "url": "/static/media/star.16b020b1.svg"
+  },
+  {
+    "revision": "fa37096ff04bb3caa35a13b775d69d25",
+    "url": "/static/media/telegram.fa37096f.svg"
+  },
+  {
+    "revision": "fb2c456d8b722f54604ad558ec8c0978",
+    "url": "/static/media/twitter-original.fb2c456d.svg"
+  },
+  {
+    "revision": "8ab1b7ff36bb6d3f0bd3df0d11a5da22",
+    "url": "/static/media/wedding.8ab1b7ff.jpg"
+  }
+]);
